d7994fc7552651fca65a068223e24e137dafe617
Moi <moisescatala@gmail.com>
2025-02-24 23:34:48 +0100

    add per ingredient request call

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True) 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""
@@ -46,7 +47,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, Igor!', icon="✅")
         
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)

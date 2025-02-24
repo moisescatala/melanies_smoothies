5eb8767913a771bcaffbeca792559765c9fc9d5a
Moi <moisescatala@gmail.com>
2025-02-24 23:30:11 +0100

    initial api integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(" My Parents New Healty Dinner  ")
@@ -44,3 +45,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, Igor!', icon="✅")
+        
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
